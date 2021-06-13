2e26ead1f68a11dcd0514de33ec0d92b3d284144
Gonzalo Sanchez <na.gonzalo@gmail.com>
2021-06-13 18:44:43 -0300

    se agregan ambas funciones de parseo

--- main.c
+++ main.c
@@ -18,9 +18,12 @@
 
 void* serial_thread(void* msg);
 void sigint_handler(int sig); /* prototype */
-void procesa_trama_serie(char* buf);
+int establish_connection(int* listener_fd, int* connected_fd);
+int procesa_trama_serie(char* buf, int len);
+int procesa_trama_tcp(char* buf, int len,int* states, int* offset);
 
 pthread_t serial_thread_hand;
+
 
 
 void sigint_handler(int sig)
@@ -35,11 +38,8 @@ int main(void)
 {
 	int ret;
 	struct sigaction sa;
-	struct addrinfo hints;
-	struct addrinfo *result;
-	int sock_fd;
+	int listener_fd, connected_fd;
 
-	
 	printf("Inicio Serial Service\r\n");
 		
 	sa.sa_handler = sigint_handler;
@@ -52,54 +52,16 @@ int main(void)
 		exit(1);
 	}
 
-	memset((void*)&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; 
-
-	int r = getaddrinfo(NULL,TCP_PORT,&hints,&result);
-	
-	if (r != 0)
+	if(establish_connection(&listener_fd, &connected_fd) ==-1)
 	{
-		perror("error getting address info");
-		exit(1);
-	}
-
-	sock_fd = socket(result->ai_family, result->ai_socktype, 0);
-	if(sock_fd == -1)
-	{
-		perror("couldn't create socket");
-		exit(1);
-	}
-	
-	if (bind(sock_fd, (struct sockaddr*)result->ai_addr,result->ai_addrlen) == -1) 
-	{
-		close(sock_fd);
-		perror("listener: bind");
-		exit(1);
-	}
-	freeaddrinfo(result);
-
-	if (listen(sock_fd, 2) == -1)
-	{
-		exit(1);
-	}
-
-	int open_sock_fd;
-	struct sockaddr_storage their_addr;
-	int addr_size = sizeof(their_addr);
-	open_sock_fd = accept(sock_fd, (struct sockaddr *)&their_addr, &addr_size);
-
-	if(open_sock_fd == -1)
-	{
-		perror("error accepting incoming conection");
+		perror("Unable to open socket connection");
 		exit(1);
 	}
 
 	if(serial_open(SERIAL_PORT,BAUD_RATE))
 	{
-		close(open_sock_fd);
-		close(sock_fd);
+		close(listener_fd);
+		close(connected_fd);
 		perror("Unable to open serial port");
 		exit(1);
 	}
@@ -114,12 +76,14 @@ int main(void)
 	ret = pthread_create(&serial_thread_hand,
 					NULL,
 					serial_thread,
-					NULL);
+					(void*)&connected_fd);
 	if(ret)
 	{
-		close(open_sock_fd);
-		close(sock_fd);
-		exit(ret);
+		close(listener_fd);
+		close(connected_fd);
+		serial_close();
+		perror("Unable to create new thread");
+		exit(1);
 	}
 
 	sigemptyset(&set);
@@ -128,10 +92,36 @@ int main(void)
 	pthread_sigmask(SIG_UNBLOCK, &set,NULL);
 
 	//Aquí procesaremos las tramas entrantes desde interface service
+	//y enviamos por puerto serie
+	char buf_in[50];
+	char buf_out[50];
+	char* ptr = buf_in;
+	int rec_bytes = 0;
+	int states[4];
+	
 	while(1)
 	{
+		rec_bytes = recv(connected_fd, ptr, sizeof(buf_in)-(ptr-buf_in),0);
 
+		if(rec_bytes <= 0)
+			break;
 
+		ptr += rec_bytes;
+		if(ptr-buf_in >= sizeof(buf_in)-1) //ver si falta un -1
+		{
+			ptr = buf_in;
+			memset(buf_in,0, sizeof(buf_in));
+		}
+
+		if( procesa_trama_tcp(buf_in, sizeof(buf_in), states, &rec_bytes) == 0)
+		{
+			sprintf(buf_out,">OUTS:%u,%u,%u,%u\r\n", states[0],states[1],states[2],states[3]);
+			
+			serial_send( buf_out, strlen(buf_out));
+		}
+		//se corre el puntero en la cantidad de bytes eliminados 
+		//por procesa_trama
+		ptr -= rec_bytes;
 	}
 	//esperamos la finalización de los hilos que lanzamos
 	pthread_join(serial_thread_hand,NULL);	
@@ -139,26 +129,201 @@ int main(void)
 	return 0;
 }
 
+/*
+Funcion que encapsula la creación y apertura de la conexión TCP.
+Por ahora solo retorna -1 en error pero se podría hacer un 
+retorno de errores mas completo
+*/
+int establish_connection(int* listener_fd, int* connected_fd)
+{
+	struct addrinfo hints;
+	struct addrinfo *result;
+	struct sockaddr_storage their_addr;
+	int addr_size;
+
+	memset((void*)&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; 
+
+	int r = getaddrinfo(NULL,TCP_PORT,&hints,&result);
+	
+	if (r != 0)
+	{		
+		return -1;
+	}
+
+	*listener_fd = socket(result->ai_family, result->ai_socktype, 0);
+	if(*listener_fd == -1)
+	{
+		return -1;
+	}
+	
+	if (bind(*listener_fd, (struct sockaddr*)result->ai_addr,result->ai_addrlen) == -1) 
+	{
+		close(*listener_fd);		
+		return -1;
+	}
+	freeaddrinfo(result);
+
+	if (listen(*listener_fd, 2) == -1)
+	{
+		return -1;
+	}
+
+	addr_size = sizeof(their_addr);
+	*connected_fd = accept(*listener_fd, (struct sockaddr *)&their_addr, &addr_size);
+
+	if(*connected_fd == -1)
+	{		
+		return -1;
+	}
+
+	return 0;
+}
+
+
 void* serial_thread(void* msg)
 {
-	char buf[50];
-	int num_bytes;
+	#define LEN_TEXT sizeof(">TOGGLE STATE:X\r\n")-1
+	int* sock_connect = (int*) msg;
+	char buf_in [LEN_TEXT * 3];
+	char buf_out[LEN_TEXT + 1];
+	char* ptr = buf_in;
+	int num_bytes = 0;
+	int button;
 
-	//aquií procesaremos las tramas entrantes desde EDUCIAA
+	memset(buf_in,0, sizeof(buf_in));
+	//aquí procesaremos las tramas entrantes desde EDUCIAA
 	while(1)
 	{
-		num_bytes = serial_receive(buf,sizeof(buf));
+		num_bytes = serial_receive(ptr,sizeof(buf_in)-(ptr-buf_in));
 		if(num_bytes <= 0)
 			break;
-		procesa_trama_serie(buf);
-		//enviar por TCP
+		
+		ptr += num_bytes;
+		if(ptr-buf_in >= sizeof(buf_in)-1)
+		{
+			ptr = buf_in;
+			memset(buf_in,0, sizeof(buf_in));
+		}
+
+		while( (button = procesa_trama_serie(buf_in, sizeof(buf_in))) != -1 )
+		{
+			ptr -= LEN_TEXT;
+			sprintf(buf_out,":LINE%uTG\n", button);
+			//aca deberíamos verificar si se envío toda la trama
+			send(*sock_connect, buf_out, strlen(buf_out), 0);			
+		}
 		usleep(SERIAL_SLEEP);
 	}
 
+	pthread_exit(NULL);
 	return NULL;
 }
 
-void procesa_trama_serie(char* buf)
+int procesa_trama_serie(char* buf, int len)
 {
+	#define LEN_TEXT sizeof(">TOGGLE STATE:X\r\n")-1
+	int index = 0;
+	char bufaux[20];
+	char* ptr;
 
+	for(index = 0; index < 4 ; index++)
+	{
+		sprintf(bufaux,">TOGGLE STATE:%u\r\n",index);
+		
+		if( (ptr = strstr(buf, bufaux)) != NULL)
+		{
+			//encontrada una trama valida desplazo todo 
+			//el buffer eliminando la trama ya procesada
+			memcpy(buf, ptr + LEN_TEXT, len - (ptr + LEN_TEXT-buf));
+			return index;;
+		}			
+	}
+	
+	return -1;	
+}
+/*
+buf buffer a proceasar
+len largo del buffer
+states puntero a un array de enteros donde se devolveran los estados a de las salidas
+offset retorna cuanto se corre el buffer
+
+retorna 0 si hay una trama correcta
+		-1 si no hay trama correcta o entera
+*/
+int procesa_trama_tcp(char* buf, int len,int* states, int* offset)
+{
+	#define LEN_STRING 12 //:STATESXYWZ\n
+	char* char_ini;
+	char* char_end;
+	int i;
+
+	*offset = 0;
+
+	char_ini = NULL;
+	//busco caracter de inicio
+	for(i = 0; i < len ; i++)
+	{
+		if(*(buf+i) == ':')
+		{
+			char_ini = buf + i;
+			break;
+		}
+	}
+	if(char_ini == NULL)
+		return -1;
+
+	//elimino todo lo que esta antes del caracter de inicio de ser necesario
+	if(buf != char_ini)
+	{
+		memcpy(buf, char_ini, len - i);
+		*offset += (int) (char_ini - buf);
+	}
+		
+
+	char_end = NULL;
+	//busco caracter de fin desde donde se encontro el de inicio
+	for(i = 0; i < len ; i++)
+	{
+		if(*(buf+i) == '\n')
+		{
+			char_end = buf + i;
+			break;
+		}
+	}
+	if(char_end == NULL)
+		return -1;
+
+	//compruebo largo de cadena
+	if(char_end - buf != LEN_STRING-1)
+	{
+		//cadena de lardo incorrecto
+		//elimino todo lo que esta antes del caracter de fin
+		memcpy(buf, char_end, len - i);
+		*offset += (int) (char_end - buf);
+		return -1;
+	}
+	//faltaria verificar el resto de la cadena
+	//verificos valores validos
+	if((buf[7] >= '0' && buf[7] <= '2') &&
+		(buf[8] >= '0' && buf[8] <= '2') &&
+		(buf[9] >= '0' && buf[9] <= '2') &&
+		(buf[10] >= '0' && buf[10] <= '2'))
+	{
+		states[0] = buf[7] - '0';
+		states[1] = buf[8] - '0';
+		states[2] = buf[9] - '0';
+		states[3] = buf[10] - '0';
+
+		memcpy(buf, char_end, len - i);
+		*offset += (int) (char_end - buf);
+		return 0;
+	}
+		
+	memcpy(buf, char_end, len - i);
+	*offset += (int) (char_end - buf);
+
+	return -1;	
 }
